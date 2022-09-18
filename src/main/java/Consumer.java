83a39942b418b19308d380c5ac6aa38383d23904
KzAlmaty <109860487+KzAlmaty@users.noreply.github.com>
2022-09-18 23:54:30 +0600

    Update Consumer.java
    Third and without mistake)))

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,12 +1,11 @@
 import org.springframework.http.*;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Consumer {
     public static void main(String[] args) {
+
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -14,68 +13,49 @@ public class Consumer {
 
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
 
-        System.out.println(responseEntity);
-
         String sessionId = String.valueOf(responseEntity.getHeaders().getFirst("Set-Cookie"));
 
-        System.out.println(sessionId);
-
-//  ======================================================================================================================
-
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Set-Cookie", sessionId);
+        headers.set("Cookie", sessionId);
+
+        System.out.println("MOYA SESSISA: " + sessionId);
+
+// =============================================================================================================
 
         Map<String, String> jsonForPOST = new HashMap<>();
         jsonForPOST.put("id", "3");
         jsonForPOST.put("name", "James");
         jsonForPOST.put("lastName", "Brown");
-        jsonForPOST.put("age", "25");
+        jsonForPOST.put("age", "20");
 
-        HttpEntity<Map<String, String>> request2 = new HttpEntity<>(jsonForPOST, headers);
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonForPOST, headers);
 
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request2, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
 
-        System.out.println(response.getBody() + "KOD OT SERVAKA");
+        System.out.println("KOD " + response.getBody());
 
-        System.out.println("======================================");
-//         Первый пароль 5ebfeb
+// =============================================================================================================
 
-// =======================================================================================================================
-//
         Map<String, String> jsonForUPDATE = new HashMap<>();
         jsonForUPDATE.put("id", "3");
         jsonForUPDATE.put("name", "Thomas");
         jsonForUPDATE.put("lastName", "Shelby");
-        jsonForUPDATE.put("age", "20");
+        jsonForUPDATE.put("age", "25");
 
-        HttpEntity<Map<String, String>> request3 = new HttpEntity<>(jsonForUPDATE, headers);
+        HttpEntity<Map<String, String>> request2 = new HttpEntity<>(jsonForUPDATE, headers);
 
-        ResponseEntity<String> response2 = restTemplate.exchange(url, HttpMethod.PUT, request3, String.class);
+        ResponseEntity<String> response2 = restTemplate.exchange(url, HttpMethod.PUT, request2, String.class);
 
-        System.out.println(response2.getBody());
+        System.out.println("KOD " + response2.getBody());
 
-        // Второй пароль cea2a2
+// =============================================================================================================
 
+        HttpEntity<String> request3 = new HttpEntity<>(headers);
 
-
-// ===============================================================================================================================
-
-//        HttpEntity<String> request4 = new HttpEntity<>(headers);
-//
-//        ResponseEntity<String> response4 = restTemplate.exchange(url + "/3", HttpMethod.DELETE, request4, String.class);
-//
-//        System.out.println(response4.getBody());
-
-
-
-
-
-
-
-
-
-
+        System.out.println("KOD " + restTemplate.exchange(url + "/3", HttpMethod.DELETE, request3, String.class).getBody());
 
 
     }
+
+
 }
